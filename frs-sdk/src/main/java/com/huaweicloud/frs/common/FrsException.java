204e5a6d19c79f9555b8a1edecb61e43f6ae7f80
FrsSvcDeveloper <liangzhiping@huawei.com>
2018-12-04 08:56:17 +0000

    Save httpcode and msg in FrsException as property

--- frs-sdk/src/main/java/com/huaweicloud/frs/common/FrsException.java
+++ frs-sdk/src/main/java/com/huaweicloud/frs/common/FrsException.java
@@ -1,11 +1,16 @@
 package com.huaweicloud.frs.common;
 
 public class FrsException extends Exception {
+    public int httpStatusCode;
+    public String msg;
+
     public FrsException() {
         super();
     }
 
     public FrsException(int httpStatusCode, String msg) {
         super(String.format("Face Recognition Service returns wrong response. HttpStatusCode: %d, Details: %s", httpStatusCode, msg));
+        this.httpStatusCode = httpStatusCode;
+        this.msg = msg;
     }
 }
